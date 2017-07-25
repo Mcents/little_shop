cdff92904290a75340bee35a8567a7530aa1444a
Michael Centrelli <Acentrelli@gmail.com>
2017-07-24 23:38:29 -0600

    Pass Test for Sessions Login

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,18 @@
+class SessionsController < ApplicationController
+
+  def new 
+  end
+
+  def create
+    @user = User.find_by(username: params[:session][:username])
+    if @user.authenticate(params[:session][:password])
+      session[:user_id] = @user.id
+      flash[:notice] = "Successful login"
+      redirect_to root_path
+    end
+  end
+
+
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root to: "users#index"
   resources :users
+
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
 end
