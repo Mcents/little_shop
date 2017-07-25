4daf7b103f20979f7629cbcc62f7d921325e1ec7
Michael Centrelli <Acentrelli@gmail.com>
2017-07-24 22:49:58 -0600

    Added View for User

---
+++ spec/features/users/user_can_create_itself_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.feature "unauthenticated user can create an account" do
+  scenario "and be re-directed to the root" do
+  user_attributes = {
+    username: "michaelc",
+    password: "mypassword"
+    }
+
+    visit new_user_path 
+    save_and_open_page
+    fill_in "user[username]", with: user_attributes[:username] 
+    fill_in "user[password]", with: user_attributes[:password]
+    click_on "Create Account"
+
+    expect(page).to have_content("Welcome, #{user.username}")
+    expect(current_path).to eq(root_path)
+  end
+end
+
