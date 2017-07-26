e342a6dde40c78dab701d787f30da32734a439d7
marcoverzero <marcoverzero@gmail.com>
2017-07-26 15:24:41 -0600

    finishes session authentication functionalities

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,14 +3,15 @@ class UsersController < ApplicationController
   def new
 
   end
-  
+
   def index
     @users = User.all
   end
 
   def create
-    @user = User.new(user_params) 
+    @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:notice] = "Welcome, #{@user.username}"
       redirect_to root_path
     else
