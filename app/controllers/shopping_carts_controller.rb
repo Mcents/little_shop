9b08988be40f69b44605967ad24e2a82e41a3af1
JF-Lalonde <lalondej88@gmail.com>
2017-07-26 17:26:11 -0600

    pass shopping cart creating test and pluralization of products

--- app/controllers/shopping_carts_controller.rb
+++ app/controllers/shopping_carts_controller.rb
@@ -1,7 +1,12 @@
 class ShoppingCartsController < ApplicationController
+  include ActionView::Helpers::TextHelper
+
   def create
-    product = Product.find(params[:product_id])
-    flash[:notice] = "1 #{product.name} added."
-    redirect_to root_path
+    id = params[:product_id].to_s
+    product = Product.find_by(id: id)
+    session[:shopping_cart] ||= {}
+    session[:shopping_cart][id] = (session[:shopping_cart][id] || 0) + 1
+    flash[:notice] = "#{pluralize(session[:shopping_cart][id], product.name)} in Cart"
+    redirect_to products_path
   end
 end
--- spec/features/shopping_carts/user_can_add_products_to_their_shopping_carts_spec.rb
+++ spec/features/shopping_carts/user_can_add_products_to_their_shopping_carts_spec.rb
@@ -13,7 +13,16 @@ RSpec.feature "When a user adds products to their shopping cart" do
     visit products_path
     click_button "Add Product"
 
-    expect(page).to have_content("1 Television added.")
+    expect(page).to have_content("1 Television in Cart")
+  end
+
+  scenario "the message updates cart amount" do
+    visit products_path
+    click_button "Add Product"
+    expect(page).to have_content("1 Television in Cart")
+    click_button "Add Product"
+
+    expect(page).to have_content("2 Televisions in Cart")
   end
 
 end
