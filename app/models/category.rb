0d235e210b101a805826486f9aad9a5d20f50db6
Michael Centrelli <Acentrelli@gmail.com>
2017-07-26 13:46:45 -0600

    Test passing for Category Model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,5 @@
 class Category < ApplicationRecord
-
+  validates :name, presence: true, uniqueness: true
+  has_many :product_categories
+  has_many :products, through: :product_categories
 end
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should have_many(:product_categories) }
+  it { should have_many(:products).through(:product_categories) }
+  it { should validate_presence_of(:name) }
+  it { should validate_uniqueness_of(:name) }
+
 end
