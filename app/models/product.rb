4ba54e69734c1f387ca9c4c7c5243d41c9111196
Michael Centrelli <Acentrelli@gmail.com>
2017-07-26 13:52:32 -0600

    Test Pass for Product Model Validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,7 @@
 class Product < ApplicationRecord
   has_many :product_categories
   has_many :categories, through: :product_categories
+  validates :name, presence: true, uniqueness: true
+  validates :description, presence: true
+  validates :price, presence: true
 end
