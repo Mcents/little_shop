1ac9493695c2c9cb4db1e891bd121795bf52eb94
Michael Centrelli <Acentrelli@gmail.com>
2017-07-26 16:59:39 -0600

    Pass Test for ProductCategory Model

--- spec/models/product_category_spec.rb
+++ spec/models/product_category_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe ProductCategory, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it {should belong_to(:category) }
+  it {should belong_to(:product) }
 end
