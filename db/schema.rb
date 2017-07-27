33c6c9c3dc9475890f16bf22ebf9e111e96f96e4
Ashish Vaid <vaidashi@Ashishs-MacBook-Air.local>
2017-07-27 08:40:48 -0600

    brands table migrated along with association to products

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170726193418) do
+ActiveRecord::Schema.define(version: 20170727050020) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "brands", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -36,6 +42,8 @@ ActiveRecord::Schema.define(version: 20170726193418) do
     t.decimal "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "brand_id"
+    t.index ["brand_id"], name: "index_products_on_brand_id"
   end
 
   create_table "users", force: :cascade do |t|
