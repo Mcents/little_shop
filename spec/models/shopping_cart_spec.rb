c3b3f13b679d503ed89a5ad687c83e2be87ac3ca
JF-Lalonde <lalondej88@gmail.com>
2017-07-26 23:55:01 -0600

    Add two methods to shopping cart: #add_product, and #count_of

--- spec/models/shopping_cart_spec.rb
+++ spec/models/shopping_cart_spec.rb
@@ -8,5 +8,20 @@ RSpec.describe ShoppingCart, type: :model do
       expect(subject.total_count).to eq(5)
     end
   end
-  
+
+  describe "#add_product" do
+    it "adds a product to its contents" do
+      subject.add_product(1)
+      subject.add_product(2)
+
+      expect(subject.contents).to eq({"1" => 3, "2" => 4})
+    end
+  end
+
+  describe "#count_of" do
+    it "reports how many of a particular item" do
+      expect(subject.count_of(1)).to eq(2)
+      expect(subject.count_of(2)).to eq(3)
+    end
+  end
 end
