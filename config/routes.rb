a3c5a364eceeb3910751454746219b42f36c15e9
JF-Lalonde <lalondej88@gmail.com>
2017-07-28 17:56:20 -0600

    Create user show page

--- config/routes.rb
+++ config/routes.rb
@@ -7,16 +7,13 @@ Rails.application.routes.draw do
   resources :categories
   resources :shopping_carts
 
-  get "/login", to: "sessions#new"
-  post "/login", to: "sessions#create"
-  delete "/logout", to: "sessions#destroy"
   get "/cart", to: "shopping_carts#index"
   put "/cart", to: "shopping_carts#update"
   delete "/update_cart", to: "shopping_carts#destroy"
 
+  get "/dashboard", to: "users#show"
   get "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
   get '/:category_name', to: 'categories#show'
-
 end
