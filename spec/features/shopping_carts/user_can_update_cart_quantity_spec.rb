5478ee084a6134671b8f527ee1bfdd1b0b86c5dd
Michael Centrelli <Acentrelli@gmail.com>
2017-07-28 00:55:29 -0600

    Fixed bug in css

--- spec/features/shopping_carts/user_can_update_cart_quantity_spec.rb
+++ spec/features/shopping_carts/user_can_update_cart_quantity_spec.rb
@@ -7,7 +7,6 @@ RSpec.feature "User can update cart quantity" do
     brand = Brand.create!(name: "Apple sickle")
     product = Product.create!(name: "Laptop1", description: "It shows moving pictures1", price: 5.00, category_ids: category.id, image_path: "https://img.bbystatic.com/BestBuy_US/images/products/1738/1738727_sa.jpg", brand_id: brand.id)
 
-
     visit products_path
     page.all(:css, '.btn-success')[0].click
     page.all(:css, '.btn-success')[0].click
