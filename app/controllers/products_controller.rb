4093a200fa90fd35f23a2e321f1024bb838acaef
JF-Lalonde <lalondej88@gmail.com>
2017-07-27 22:36:43 -0600

    Add edit action to products controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,45 +1,40 @@
 class ProductsController < ApplicationController
 
-  def new
-  end
+def new
+end
 
-  def create
-    @product = Product.new(product_params)
-    if @product.save
-      redirect_to products_path
-    else
-      render :new
-    end
-  end
-
-  def create
-    @categories = Category.find_by(params[:category_ids])
-    @product = @categories.products.new(product_params)
-    if @product.save
-      redirect_to products_path
-    else
-      render :new
-    end
-  end
-
-  def show
-    @product = Product.find(params[:id])
-  end
-
-  def index
-    @products = Product.all
-  end
-
-  def destroy
-    @product = Product.find(params[:id])
-    @product.destroy
-
+def create
+  @product = Product.new(product_params)
+  if @product.save
     redirect_to products_path
+  else
+    render :new
   end
+end
 
-  private
+def show
+  @product = Product.find(params[:id])
+end
 
-  def product_params
-    params.require(:product).permit(:name, :description, :price, :brand_id, category_ids: [])
-  end
+def edit
+  @product = Product.find(params[:id])
+end
+
+def index
+  @products = Product.all
+end
+
+def destroy
+  @product = Product.find(params[:id])
+  @product.destroy
+
+  redirect_to products_path
+end
+
+private
+
+def product_params
+  params.require(:product).permit(:name, :description, :price)
+end
+
 end
