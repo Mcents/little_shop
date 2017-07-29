644b0d7495beb96306606ec597c454e6d455c069
Michael Centrelli <Acentrelli@gmail.com>
2017-07-28 22:21:12 -0600

    Added Model Test for User Role

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
   has_secure_password
   validates :username, presence: true, uniqueness: true
   validates :password, presence: true
+
+  enum role: %w(default admin)
 end
