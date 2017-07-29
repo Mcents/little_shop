5ea2606cadd09719ffbbd4468d2afb5f4bc07f8b
Michael Centrelli <Acentrelli@gmail.com>
2017-07-28 23:18:22 -0600

    Pass Test for Admin Visiting Dashboard

--- spec/features/admins/admin_only_can_visit_admin_dashboard_spec.rb
+++ spec/features/admins/admin_only_can_visit_admin_dashboard_spec.rb
@@ -2,14 +2,13 @@ require 'rails_helper'
 
 RSpec.feature "Admin can access the admin dashboard" do
   scenario "only when logged in with admin privledges" do
-    user = User.create(username: "michael", password: "password")
-    user.admin!
+    user = User.create(username: "michael", password: "password", role: 1)
 
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
     visit "/admin/dashboard"
 
-    expect(current_path).to eq(admin_dashbaord_path)
+    expect(current_path).to eq(admin_dashboard_path)
     expect(page).to have_content("Admin Dashboard")
 
 
