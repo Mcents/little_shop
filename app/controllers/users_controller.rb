a513d858532c5d6eaa687cf45ffbf5afc24a1482
Michael Centrelli <Acentrelli@gmail.com>
2017-07-28 23:52:19 -0600

    Fixed Routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,14 +20,9 @@ class UsersController < ApplicationController
     end
   end
 
-
   private
 
   def user_params
     params.require(:user).permit(:username, :password)
   end
-
-
-
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   namespace :admin do
     get '/dashboard', to: 'dashboard#index'
     get '/dashboard/:id', to: 'dashboard#show', as: 'order'
-    resources :trips
+    resources :products
   end
 
   resources :users, except: [:show]
