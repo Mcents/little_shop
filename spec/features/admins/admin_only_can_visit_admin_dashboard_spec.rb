42fc13446896612cbb174aa3211079b3f97cc47c
Michael Centrelli <Acentrelli@gmail.com>
2017-07-28 23:26:06 -0600

    Pass Test for 404 On User Admin Dashboard

--- spec/features/admins/admin_only_can_visit_admin_dashboard_spec.rb
+++ spec/features/admins/admin_only_can_visit_admin_dashboard_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.feature "Admin can access the admin dashboard" do
-  scenario "only when logged in with admin privledges" do
+  scenario "and see the admin dashboard" do
     user = User.create(username: "michael", password: "password", role: 1)
 
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
@@ -10,7 +10,14 @@ RSpec.feature "Admin can access the admin dashboard" do
 
     expect(current_path).to eq(admin_dashboard_path)
     expect(page).to have_content("Admin Dashboard")
+  end
+end
+describe "as a registered user when I visit /admin/dashboard" do
+  scenario "I see a 404" do
+    user = User.create(username: "mikeey", password: "passey", role: 0)
 
+    visit '/admin/dashboard'
 
+    expect(page).to have_content("The page you were looking for doesn't exist.")
   end
 end
