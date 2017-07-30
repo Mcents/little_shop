ff2e091dea6b76b32870df24dacf904165a5ed71
JF-Lalonde <lalondej88@gmail.com>
2017-07-30 00:03:40 -0600

    Add sad path test to product show test

--- spec/features/products/user_can_view_a_single_product_spec.rb
+++ spec/features/products/user_can_view_a_single_product_spec.rb
@@ -12,4 +12,15 @@ RSpec.feature "User can view an individual product" do
     expect(page).to have_content(product1.price)
 
   end
+
+  scenario "but cannot view retired button if item is active" do
+    brand = Brand.create!(name: "ToysRus")
+    product = Product.create!(name: "Magic Mirror", description: "Show gifs", brand: brand, price: 45.99, image_path: "http://www.murphysmagicmirror.co.uk/wp-content/uploads/2017/01/MagicMirror_15.jpg?gid=1", role: 0)
+
+    visit product_path(product)
+
+    expect(page).to have_button("Add Product")
+    expect(page).to_not have_button("Item Retired")
+  end
+
 end
