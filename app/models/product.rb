6c39243e37e268c790d0c8244b6d26d08ebdf25a
JF-Lalonde <lalondej88@gmail.com>
2017-07-29 23:22:34 -0600

    Add role column to products and create model tests

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,9 +2,10 @@ class Product < ApplicationRecord
 
   has_many :product_categories
   has_many :categories, through: :product_categories
-  belongs_to :brand 
+  belongs_to :brand
   validates :name, presence: true, uniqueness: true
   validates :description, presence: true
   validates :price, presence: true
 
+  enum role: %w(active retired)
 end
