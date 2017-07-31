b650dd8b9e9075fcb71ba260069dce6d0976cd8e
marcoverzero <marcoverzero@gmail.com>
2017-07-30 18:42:10 -0600

    basically passes user story 10. room for more precision capybara

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,7 +19,6 @@ class UsersController < ApplicationController
     if @user.save
       session[:user_id] = @user.id
       flash[:notice] = "Welcome, #{@user.username}"
-      binding.pry
       redirect_to "/dashboard"
     else
       flash[:notice] = "Invalid credentials"
--- spec/features/guests/guest_user_can_only_checkout_after_login_spec.rb
+++ spec/features/guests/guest_user_can_only_checkout_after_login_spec.rb
@@ -10,23 +10,33 @@ RSpec.feature "As a guest user" do
     brand = Brand.create!(name: "Apple sickle")
     product_1 = Product.create!(id: 1, name: "Laptop1", description: "It shows moving pictures1", price: 5.00, image_path: "https://img.bbystatic.com/BestBuy_US/images/products/1738/1738727_sa.jpg", brand_id: brand.id)
     product_2 = Product.create!(id: 2,name: "Laptop2", description: "It shows moving pictures2", price: 15.00, image_path: "https://img.bbystatic.com/BestBuy_US/images/products/1738/1738727_sa.jpg", brand_id: brand.id)
-    cart = ShoppingCart.new({"1" => 2, "2" => 3})
+    # cart = ShoppingCart.new({"1" => 2, "2" => 3})
 
-    visit cart_path
+    visit product_path(product_1)
+    click_button("Add Product")
+    click_on("Your Cart")
+
+    expect(current_path).to eq("/cart")
+
 
     expect(page).to_not have_content("Checkout")
     #refactor ^^ to look for css tag on checkout button
     #refactor vv to "Login or Create Account to Checkout"
-    expect(page).to_not have_content("Login or Create Account to ")
+    expect(page).to have_content("Login")
 
     page.all(:css, '.login-navbar-unique')[0].click
+
     click_on "Create an Account"
+    
     fill_in "user[username]", with: user_attributes[:username]
     fill_in "user[password]", with: user_attributes[:password]
+    fill_in "user[confirm_password]", with: user_attributes[:password]
+    click_on "Create Account"
+
     visit cart_path
 
-    expect(page).to have_content("laptop1")
-    expect(page).to have_content("laptop2")
-    expect(session[:cart][:contents]).to eq({"1" => 2, "2" => 3})
+    expect(page).to have_content("Laptop1")
+    expect(page).to_not have_content("Laptop2")
+    expect(page).to have_content("Your Cart 1")
   end
 end
