ab488002fff02d0f85c62bd302c3f05185fc85c7
JF-Lalonde <lalondej88@gmail.com>
2017-07-31 13:06:51 -0600

    Refactor test for card 16

--- spec/features/users/users_security_spec.rb
+++ spec/features/users/users_security_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
 RSpec.feature "Users cannot view other users' private data" do
-  scenario "as authenticated users" do
-    user = User.create!(username: "Timmy", password: "123")
-    user2 = User.create!(username: "Jimmy", password: "123")
+  before(:each) do
+    @user = User.create!(username: "Timmy", password: "123")
+    @user2 = User.create!(username: "Jimmy", password: "123")
 
     brand = Brand.create!(name: "Sony")
 
@@ -11,28 +11,29 @@ RSpec.feature "Users cannot view other users' private data" do
     product2 = Product.create!(name: "prod2", description: "Also sick", price: 200, brand_id: brand.id)
     product3 = Product.find_by(name: "prod2")
 
-    order1 = Order.create!(user_id: user.id)
-    order2 = Order.create!(user_id: user2.id)
+    @order1 = Order.create!(user_id: @user.id)
+    @order2 = Order.create!(user_id: @user2.id)
 
-    order1.products << [product1, product2, product3]
+    @order1.products << [product1, product2, product3]
 
-    order2.products << [product2, product3]
+    @order2.products << [product2, product3]
+  end
+  scenario "as authenticated users" do
 
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
 
-    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+    visit user_path(@user)
+    expect(page).to have_content("Welcome #{@user.username}")
 
-    visit user_path(user)
-    expect(page).to have_content("Welcome #{user.username}")
+    visit user_path(@user2)
+    expect(page).to_not have_content("Welcome #{@user2.username}")
 
-    visit user_path(user2)
-    expect(page).to_not have_content("Welcome #{user2.username}")
-
-    visit edit_user_path(user2)
+    visit edit_user_path(@user2)
     expect(page).to have_content("The page you were looking for doesn't exist.")
 
     visit orders_path
-    expect(page).to have_content(order1.id)
-    expect(page).to_not have_content(order2.id)
+    expect(page).to have_content(@order1.id)
+    expect(page).to_not have_content(@order2.id)
 
     visit admin_dashboard_path
     expect(page).to have_content("The page you were looking for doesn't exist.")
@@ -40,7 +41,32 @@ RSpec.feature "Users cannot view other users' private data" do
     visit admin_products_path
     expect(page).to have_content("The page you were looking for doesn't exist.")
 
-    visit edit_user_path(user)
+    visit edit_user_path(@user)
+    expect(page).to_not have_content("Role")
+  end
+
+  scenario "as unauthenticated users" do
+
+    visit user_path(@user)
+    expect(page).to have_content("Welcome #{@user.username}")
+
+    visit user_path(@user2)
+    expect(page).to_not have_content("Welcome #{@user2.username}")
+
+    visit edit_user_path(@user2)
+    expect(page).to have_content("The page you were looking for doesn't exist.")
+
+    visit orders_path
+    expect(page).to have_content(@order1.id)
+    expect(page).to_not have_content(@order2.id)
+
+    visit admin_dashboard_path
+    expect(page).to have_content("The page you were looking for doesn't exist.")
+
+    visit admin_products_path
+    expect(page).to have_content("The page you were looking for doesn't exist.")
+
+    visit edit_user_path(@user)
     expect(page).to_not have_content("Role")
   end
 end
