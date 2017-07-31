a0be330fc3f6679f8dc3b5eb50360a7a76b7330b
Michael Centrelli <Acentrelli@gmail.com>
2017-07-31 14:06:54 -0600

    Added Function to Filter Orders by Status -Buttons for changing order status

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,10 +1,25 @@
 class Admin::DashboardController <Admin::BaseController
-
+ 
   def index
-    @orders = Order.all 
+    if params.include?("status")
+      @order = Order.where(status: params[:status])
+      render :show
+    else
+      @orders = Order.all 
+    end
   end
 
+  def edit_status
+    @order = Order.find(params[:order_id])
+      if @order.status == 'ordered'
+      @order.update_attribute :status, 1
 
+      redirect_back(fallback_location: admin_dashboard_path)
+    elsif @order.status == 'paid'
+      @order.update_attribute :status, 2
 
+      redirect_back(fallback_location: admin_dashboard_path)
+    end
+  end
 
 end
