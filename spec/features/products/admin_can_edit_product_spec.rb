c8c2f591cf747e01b084aa5e68f2001e2d4b818a
JF-Lalonde <lalondej88@gmail.com>
2017-08-01 17:34:55 -0600

    Link shopping cart in navbar and fix test in admin dashboard

--- spec/features/products/admin_can_edit_product_spec.rb
+++ spec/features/products/admin_can_edit_product_spec.rb
@@ -21,7 +21,7 @@ RSpec.feature "Admin can edit a product" do
     expect(page).to_not have_content("Laptop4")
   end
 
-  scenario "with invalid attributes" do 
+  scenario "with invalid attributes" do
     brand = Brand.create!(name: "microsoft")
     product = Product.create!(name: "Laptop4", description: "It shows moving pictures", price: 5.00, image_path: "https://img.bbystatic.com/BestBuy_US/images/products/1738/1738727_sa.jpg", brand_id: brand.id)
 
@@ -32,7 +32,6 @@ RSpec.feature "Admin can edit a product" do
 
 
     click_button "Update Product"
-    save_and_open_page
 
     expect(page).to have_content("Invalid Credentials")
   end
