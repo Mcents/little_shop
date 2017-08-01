10dcca1c2c3543a626c571964babc6d179a75281
marcoverzero <marcoverzero@gmail.com>
2017-08-01 14:32:02 -0600

    adds user edit sad path test

--- spec/controllers/user_controller_spec.rb
+++ spec/controllers/user_controller_spec.rb
@@ -1,20 +1,36 @@
 require 'rails_helper'
 
-RSpec.feature "User cannot edit their profile badly" do
-  scenario "and see their updated information" do
+RSpec.feature "User cannot edit their profile" do
+  scenario "without a password" do
     user1 = User.create(username: "usericus", password: "password", role: 0)
     visit login_path
-    fill_in "session[username]", with: "micha"
-    fill_in "session[password]", with: "guy"     
-   page.all(:css, '.login-button-unique')[0].click
+    fill_in "session[username]", with: "usericus"
+    fill_in "session[password]", with: "password"
+    page.all(:css, '.login-button-unique')[0].click
 
-    visit user_path(user1)
+    visit dashboard_path
+    # save_and_open_page
     click_on "Edit User"
     fill_in "user[username]", with: "newuser"
-    fill_in "user[password]", with: user1.password
     click_on "Update Account"
 
-    expect(current_path).to eq(dashboard_path)
-    expect(page).to have_content("Welcome newuser")
+    expect(current_path).to eq(user_path(user1))
+  end
+
+  scenario "without a username" do
+    user1 = User.create(username: "usericus", password: "password", role: 0)
+    visit login_path
+    fill_in "session[username]", with: "usericus"
+    fill_in "session[password]", with: "password"
+    page.all(:css, '.login-button-unique')[0].click
+
+    visit dashboard_path
+    # save_and_open_page
+    click_on "Edit User"
+    fill_in "user[username]", with: ""
+    fill_in "user[password]", with: "password"
+    click_on "Update Account"
+
+    expect(current_path).to eq(user_path(user1))
   end
 end
