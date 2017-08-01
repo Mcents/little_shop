4a8f0070a61203d999ef0a15b16fe1a149e21358
JF-Lalonde <lalondej88@gmail.com>
2017-08-01 16:44:30 -0600

    Link categories in navbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@
   protect_from_forgery with: :exception
   helper_method :current_user
   before_action :set_shopping_cart
+  before_action :all_categories
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
@@ -13,5 +14,9 @@
 
   def current_admin?
     current_user && current_user.admin?
+  end
+
+  def all_categories
+    @categories = Category.all
   end
 end
