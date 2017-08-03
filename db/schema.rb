6a4bb59974d6c493a14f931137c4c0e8845239f8
Michael Centrelli <Acentrelli@gmail.com>
2017-08-02 19:07:00 -0600

    Fixed Everything

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170731230156) do
+ActiveRecord::Schema.define(version: 20170801234407) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -73,6 +73,9 @@ ActiveRecord::Schema.define(version: 20170731230156) do
     t.integer "role"
     t.string "full_name"
     t.string "address"
+    t.string "uid"
+    t.string "provider"
+    t.string "oauth_token"
   end
 
   add_foreign_key "order_products", "orders"
